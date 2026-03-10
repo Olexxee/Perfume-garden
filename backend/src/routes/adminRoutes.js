b2049f14aaf681c602e963d987fb7ae450478be0
Olexxee <olexxee14@gmail.com>
2026-03-10 06:11:22 -0700

    Seperated the backend file

--- backend/src/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginAdmin } from "../controller/adminController.js";
-
-const router = express.Router();
-
-router.post("/login", loginAdmin);
-
-export default router;
